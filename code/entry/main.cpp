d5b863873bf1c59f4b30ddf4ce1f46af5d0e440a
oh-facts <as17793@nyu.edu>
2024-04-24 03:53:31 -0400

    window stuff

--- code/entry/main.cpp
+++ code/entry/main.cpp
@@ -7,6 +7,31 @@
 #include <third_party/stb_sprintf.h>
 #include <third_party/HandmadeMath.h>
 
+extern "C"
+{
+#pragma function(memset)
+  void *memset(void *dest, int c, size_t count)
+  {
+    char *bytes = (char *)dest;
+    while (count--)
+    {
+      *bytes++ = (char)c;
+    }
+    return dest;
+  }
+  
+#pragma function(memcpy)
+  void *memcpy(void *dest, const void *src, size_t count)
+  {
+    char *dest8 = (char *)dest;
+    const char *src8 = (const char *)src;
+    while (count--)
+    {
+      *dest8++ = *src8++;
+    }
+    return dest;
+  }
+}
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -57,7 +82,6 @@ void print(const char *format, ...)
 
 int __stdcall mainCRTStartup()
 {
-  
   HMM_Vec2 a = {1,2};
   HMM_Vec2 b = {2,3};
   
@@ -67,18 +91,32 @@ int __stdcall mainCRTStartup()
   print(" ");
   print("said hi");
   
-  if (3 < 4)
+  WNDCLASSA wc = {};
+  
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.lpfnWndProc = WndProc;
+  wc.hInstance = GetModuleHandle(0);
+  wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+  wc.lpszClassName = "main";
+  
+  
+  RegisterClass(&wc);
+  
+  HWND win = CreateWindowA(wc.lpszClassName, "window",WS_OVERLAPPED, 100,100, 800, 600, 0, 0, wc.hInstance, 0);
+  
+  if(!win)
   {
-    AssertM(false , "wow");
+    print("where is window");
   }
   
+  ShowWindow(win, SW_SHOWNORMAL);
+  UpdateWindow(win);
+  
+  while(1)
+  {
+    
+  }
+  
+  
   ExitProcess(0);
-  
-  ID3D12Device* device;
-  IDXGISwapChain3* swapChain;
-  
-  // Create the device
-  D3D12CreateDevice(0, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device));
-  
-  return 0;
 }
